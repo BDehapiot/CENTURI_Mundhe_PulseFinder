e0ec0217a94817107c5d6c125dbcfca0b540109d
bdehapiot <b.dehapiot@gmail.com>
2022-03-31 17:24:55 +0200

    update

--- pulse_finder/tasks.py
+++ pulse_finder/tasks.py
@@ -2,14 +2,17 @@
 
 import os
 import cv2
-import napari
 import numpy as np
+
+from skimage import io
+from skimage.filters import gaussian
+
+import napari
+from magicgui import magicgui
 
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvas
-
-from skimage import io
 
 #%%
 
@@ -72,6 +75,87 @@ for i in cell_id:
     
 #%%
 
+# i = 5
+
+# # Extract cell_data 
+# time = cell_data[i-1]['time'].astype('int')
+# ctrd_x = cell_data[i-1]['ctrd_x'].astype('int')
+# ctrd_y = cell_data[i-1]['ctrd_y'].astype('int')
+# myoii_mean = cell_data[i-1]['myoii_mean']
+# myoii_intden = cell_data[i-1]['myoii_intden']
+# area = cell_data[i-1]['area']
+
+# # Create empty display 
+# display = np.zeros([time.shape[0], CROP_Y, CROP_X])
+
+# for t in range(time.shape[0]):
+
+#     # Set crop limits
+#     y1 = ctrd_y[t] - CROP_Y//2
+#     y2 = ctrd_y[t] + CROP_Y//2
+#     x1 = ctrd_x[t] - CROP_X//2
+#     x2 = ctrd_x[t] + CROP_X//2
+    
+#     # Crop data 
+#     myoii_crop = myoii[time[t]-1,y1:y2,x1:x2]     
+#     labels_crop = labels[time[t]-1,y1:y2,x1:x2] == i
+#     outline_crop = (pixconn(labels_crop, 2) < 8) * labels_crop
+    
+#     # Draw text
+#     font = cv2.FONT_HERSHEY_DUPLEX
+#     text = 'Cell #' + str(i) + ' Time = ' + str(time[t])
+#     text_crop = np.zeros(outline_crop.shape)    
+#     text_crop = cv2.putText(text_crop, text, (10,25), font, 0.5, (1,1,1), 1, cv2.LINE_AA)   
+    
+#     # Append display
+#     display[t,0:myoii_crop.shape[0],0:myoii_crop.shape[1]] = myoii_crop + (outline_crop + text_crop)*255
+
+# # Show results in napari viewer
+# with napari.gui_qt():
+
+#     # Plot data
+#     static_canvas = FigureCanvas(Figure(figsize=(5, 5)))
+    
+#     ax1 = static_canvas.figure.subplots()    
+#     line1, = ax1.plot(time, myoii_intden, color='blue', label='MyoII')
+#     ax1.axvline(x=60)
+#     ax1.set_xlabel('time_point')
+#     ax1.set_ylabel('MyoII Int. Den. (A.U.)')
+    
+#     ax2 = ax1.twinx()    
+#     line2, = ax2.plot(time, area, color='gray', linestyle='dashed', label='area')
+#     ax2.set_xlabel('time_point')
+#     ax2.set_ylabel('cell area (pixels)')    
+    
+#     ax1.set_title('Cell #' + str(i) + ' MyoII & cell area')
+#     ax1.legend(handles=[line1, line2])
+    
+#     # Add to napari viewer
+    
+#     @magicgui(
+#         auto_call=True,
+#         call_button="select pulse(s)")
+    
+#     def select_pulses(
+#             data: napari.types.ImageData, 
+#             pulse1_ti: int = None,
+#             pulse1_tf: int = None,
+#             pulse2_ti: int = None,
+#             pulse2_tf: int = None,
+#             pulse3_ti: int = None,
+#             pulse3_tf: int = None,
+#             pulse4_ti: int = None,
+#             pulse4_tf: int = None
+#         ) -> napari.types.ImageData:
+#         return data
+    
+#     viewer = napari.Viewer()
+#     viewer.add_image(display, name='Cell #' + str(i) + ' MyoII', contrast_limits = [0, np.quantile(myoii, 0.999)])
+#     viewer.window.add_dock_widget(static_canvas, area='bottom', name='plot')
+#     viewer.window.add_dock_widget(select_pulses, area='right', name='widget')  
+        
+#%%
+
 i = 5
 
 # Extract cell_data 
@@ -106,38 +190,47 @@ for t in range(time.shape[0]):
     
     # Append display
     display[t,0:myoii_crop.shape[0],0:myoii_crop.shape[1]] = myoii_crop + (outline_crop + text_crop)*255
-
-# Show results in napari viewer
-with napari.gui_qt():
-
-    # Plot data
-    static_canvas = FigureCanvas(Figure(figsize=(5, 5)))
-    
-    ax1 = static_canvas.figure.subplots()    
-    line1, = ax1.plot(time, myoii_intden, color='blue', label='MyoII')
-    ax1.set_xlabel('time_point')
-    ax1.set_ylabel('MyoII Int. Den. (A.U.)')
-    
-    ax2 = ax1.twinx()    
-    line2, = ax2.plot(time, area, color='gray', linestyle='dashed', label='area')
-    ax2.set_xlabel('time_point')
-    ax2.set_ylabel('cell area (pixels)')    
-    
-    ax1.set_title('Cell #' + str(i) + ' MyoII & cell area')
-    ax1.legend(handles=[line1, line2])
-    
+   
     # Add to napari viewer
-    viewer = napari.Viewer()
-    viewer.add_image(display, name='Cell #' + str(i) + ' MyoII', contrast_limits = [0, np.quantile(myoii, 0.999)])
-    viewer.window.add_dock_widget(static_canvas, area='bottom', name='plot') 
     
+@magicgui(
+    auto_call=True,
+    call_button="select pulse(s)")
+
+def select_pulses( 
+        pulse1_ti: int = None,
+        pulse1_tf: int = None,
+        pulse2_ti: int = None,
+        pulse2_tf: int = None,
+        pulse3_ti: int = None,
+        pulse3_tf: int = None,
+        pulse4_ti: int = None,
+        pulse4_tf: int = None
+    ) -> napari.types.ImageData:
+    
+    # Show results in napari viewer
+    with napari.gui_qt():
+
+        # Plot data
+        static_canvas = FigureCanvas(Figure(figsize=(5, 5)))
         
-#%%
-
-# # Display data in napari
-# viewer = napari.Viewer()
-# viewer.add_image(
-#     display, name="display", contrast_limits = [0, np.quantile(myoii, 0.999)])
-
-# viewer.add_image(
-#     labels, name="labels")
+        ax1 = static_canvas.figure.subplots()    
+        line1, = ax1.plot(time, myoii_intden, color='blue', label='MyoII')
+        ax1.axvline(x=60)
+        ax1.set_xlabel('time_point')
+        ax1.set_ylabel('MyoII Int. Den. (A.U.)')
+        
+        ax2 = ax1.twinx()    
+        line2, = ax2.plot(time, area, color='gray', linestyle='dashed', label='area')
+        ax2.set_xlabel('time_point')
+        ax2.set_ylabel('cell area (pixels)')    
+        
+        ax1.set_title('Cell #' + str(i) + ' MyoII & cell area')
+        ax1.legend(handles=[line1, line2])
+    
+    return static_canvas
+    
+viewer = napari.Viewer()
+viewer.add_image(display, name='Cell #' + str(i) + ' MyoII', contrast_limits = [0, np.quantile(myoii, 0.999)])
+viewer.window.add_dock_widget(static_canvas, area='bottom', name='plot')
+viewer.window.add_dock_widget(select_pulses, area='right', name='widget')  
