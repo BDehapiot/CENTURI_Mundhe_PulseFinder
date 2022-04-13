630b066516d1e79198a31b0ca9ee40f1634dab34
Benoit Dehapiot <b.dehapiot@gmail.com>
2022-04-13 14:51:06 +0200

    update

---
+++ pulse_finder/functions.py
@@ -0,0 +1,151 @@
+#%%
+
+import os
+import cv2
+import time
+import numpy as np
+
+from joblib import Parallel, delayed 
+
+#%%
+
+from tools.conn import pixconn
+
+#%%
+
+def get_cell_data(
+        myoii, labels, cell_info_path, all_id, crop_y, crop_x, parallel=True):
+    
+    ''' General description.
+    
+    Parameters
+    ----------    
+    myoii : np.ndarray
+        Description.
+        
+    labels : np.ndarray
+        Description.
+        
+    cell_info_path : str
+        Description.
+        
+    all_id : np.ndarray
+        Description.
+        
+    crop_y : int
+        Description.
+        
+    crop_x : int   
+        Description.
+        
+    parallel : bool
+        Description.
+        
+    Returns
+    -------  
+    cell_data : list
+        Description.    
+        
+    Notes
+    -----   
+    
+    '''
+    
+    def _get_cell_data(cell_id):
+        
+        cell_info_list = os.listdir(cell_info_path)
+                
+        for file_name in cell_info_list:
+            
+            if 'Cell_' + str(cell_id) + '_info' in file_name:
+                
+                temp_data_cell = np.loadtxt(
+                    cell_info_path + '/' + file_name, skiprows=1)
+    
+            if 'Cell_' + str(cell_id) + '_I_BGsub' in file_name:
+                
+                temp_data_myoii = np.loadtxt(
+                    cell_info_path + '/' + file_name, skiprows=1)  
+             
+        # Get variables        
+        timepoint = temp_data_cell[:,12].astype('int')
+        ctrd_x = temp_data_cell[:,2].astype('int')
+        ctrd_y = temp_data_cell[:,3].astype('int')
+        area = temp_data_cell[:,1]
+        myoii_mean = temp_data_myoii[:,1]
+        myoii_intden = temp_data_myoii[:,7]
+                
+        # Create cell_display 
+        cell_display = np.zeros([timepoint.shape[0], crop_y, crop_x]).astype('int')
+        
+        for t in range(timepoint.shape[0]):
+        
+            # Set crop limits
+            y1 = ctrd_y[t] - crop_y//2
+            y2 = ctrd_y[t] + crop_y//2
+            x1 = ctrd_x[t] - crop_x//2
+            x2 = ctrd_x[t] + crop_x//2
+            
+            # Crop data 
+            myoii_crop = myoii[timepoint[t]-1,y1:y2,x1:x2] 
+            labels_crop = labels[timepoint[t]-1,y1:y2,x1:x2] == cell_id
+            outline_crop = (pixconn(labels_crop, 2) < 8) * labels_crop
+            
+            # Draw text
+            font = cv2.FONT_HERSHEY_DUPLEX
+            text = 'Cell #' + str(cell_id) + ' timepoint = ' + str(timepoint[t])
+            text_crop = np.zeros(outline_crop.shape)    
+            text_crop = cv2.putText(
+                text_crop, text, (10,25), font, 0.5, (1,1,1), 1, cv2.LINE_AA)   
+            
+            # Append cell_display
+            cell_display[t,0:myoii_crop.shape[0],0:myoii_crop.shape[1]] = myoii_crop + (outline_crop + text_crop)*255
+                
+        # Append all_data list                        
+        cell_data = {
+            'cell_id' : cell_id,
+            'timepoint' : timepoint,
+            'area' : area,
+            'ctrd_x' : ctrd_x,
+            'ctrd_y' : ctrd_y,
+            'myoii_mean' : myoii_mean,
+            'myoii_intden' : myoii_intden,
+            'display' : cell_display
+            }  
+                    
+        return cell_data
+    
+    # -------------------------------------------------------------------------
+    
+    if parallel:
+        
+        start = time.time()
+        print('get_cell_data (parallel)')
+    
+        # Run _get_cell_data (parallel)
+        cell_data = Parallel(n_jobs=-1)(
+            delayed(_get_cell_data)(
+                cell_id
+                )
+            for cell_id in all_id
+            )
+        
+        end = time.time()
+        print(f'  {(end - start):5.3f} s')
+        
+    else:
+        
+        start = time.time()
+        print('get_cell_data')
+        
+        # Run _get_cell_data
+        cell_data = [_get_cell_data(
+                cell_id
+                )
+            for cell_id in all_id
+            ]
+        
+        end = time.time()
+        print(f'  {(end - start):5.3f} s')
+        
+    return cell_data
---
+++ pulse_finder/tasks_current2.py
@@ -0,0 +1,200 @@
+#%%
+
+import os
+import cv2
+import time
+import napari
+import numpy as np
+
+from skimage import io
+
+from magicgui import magicgui
+
+from joblib import Parallel, delayed 
+
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvas
+
+#%%
+
+from functions import get_cell_data
+
+#%%
+
+# Inputs
+DATA_PATH = '../data/'
+FOLD_NAME = 'ecadgfp_sqhmch_200820_e4_controlforhkbfog'
+CROP_Y = 250; CROP_X = 400
+
+# Manage paths
+myoii_path = DATA_PATH + '/' + FOLD_NAME + '/' + FOLD_NAME + '_MyoII_MAX.tif'
+labels_path = DATA_PATH + '/' + FOLD_NAME + '/' + FOLD_NAME + '_cell_tracks.tif'
+cell_info_path = DATA_PATH + '/' + FOLD_NAME + '/cell_info'
+
+# Open data
+myoii = io.imread(myoii_path) 
+labels = io.imread(labels_path)  
+
+# Get variables
+nT = myoii.shape[0]
+nY = myoii.shape[1]
+nX = myoii.shape[2]
+
+# Get unique cell id
+all_id = np.arange(1, np.max(labels)+1) # works with continuous labels
+
+# Define cellViewer class
+class cellViewer(napari.Viewer):
+    pass
+
+#%%    
+
+cell_data = get_cell_data(
+    myoii, labels, cell_info_path, all_id, CROP_Y, CROP_X) 
+
+cellViewer.cell_data = cell_data[0]
+cellViewer.pulse_data = [None] * len(all_id)
+
+# -----------------------------------------------------------------------------     
+
+cell_fig = plt.figure()
+
+ax1 = cell_fig.add_subplot()
+line1, = ax1.plot(
+    cell_data[0]['timepoint'], cell_data[0]['myoii_intden'], 
+    color='blue', label='MyoII'
+    )
+
+ax1.set_xlabel('Timepoint')
+ax1.set_ylabel('MyoII Int. Den. (A.U.)')
+
+ax2 = ax1.twinx()
+line2, = ax2.plot(
+    cell_data[0]['timepoint'], cell_data[0]['area'],
+    color='gray', linestyle='dashed', label='area'
+    ) 
+
+ax2.set_xlabel('Timepoint')
+ax2.set_ylabel('Cell area (pixels)')  
+
+ax1.set_title('Cell #' + str(1) + ' MyoII & cell area')
+ax1.legend(handles=[line1, line2])
+
+p1_ti = ax1.twinx(); p1_ti.axis('off')
+p1_tf = ax1.twinx(); p1_tf.axis('off')
+
+# -----------------------------------------------------------------------------  
+
+#%%
+
+@magicgui(
+    
+    auto_call = True,
+    
+    next_cell = {
+        'widget_type': 'PushButton',
+        'value': False, 
+        'label': 'next cell'
+        },
+    
+    pulse1_ti = {
+        'widget_type': 'Slider', 
+        'min': np.min(cellViewer.cell_data['timepoint'])-1, 
+        'max': np.max(cellViewer.cell_data['timepoint'])
+        },
+    
+    pulse1_tf = {
+        'widget_type': 'Slider', 
+        'min': np.min(cellViewer.cell_data['timepoint'])-1, 
+        'max': np.max(cellViewer.cell_data['timepoint'])
+        }, 
+
+    )
+
+def plot_data(
+        next_cell: bool,
+        pulse1_ti: int,
+        pulse1_tf: int,
+        ):        
+                        
+    # -------------------------------------------------------------------------    
+    
+    if pulse1_ti >= np.min(cellViewer.cell_data['timepoint']): 
+        p1_ti.clear()
+        p1_ti.axvline(x=pulse1_ti)
+        p1_ti.text(pulse1_ti+0.25,0.77,'p1_ti',rotation=90)
+        p1_ti.axis('off')
+        pulse1_ti = plot_data.pulse1_ti.value
+    else:
+        p1_ti.clear()
+        p1_ti.axis('off')
+    
+    if pulse1_tf >= pulse1_ti: 
+        p1_tf.clear()
+        p1_tf.axvline(x=pulse1_tf)
+        p1_ti.text(pulse1_tf+0.25,0.77,'p1_tf',rotation=90)
+        p1_tf.axis('off')
+        pulse1_tf = plot_data.pulse1_tf.value
+    else:
+        p1_tf.clear()
+        p1_tf.axis('off')
+        
+    cell_fig.canvas.draw()
+    
+    if plot_data.pulse1_ti.value > np.min(cellViewer.cell_data['timepoint'])-1:
+        cellViewer.pulse_data[cellViewer.cell_data['cell_id']-1] = (plot_data.pulse1_ti.value, plot_data.pulse1_tf.value)
+
+@plot_data.next_cell.changed.connect  
+def update_slider():
+    
+    cellViewer.cell_data = cell_data[cellViewer.cell_data['cell_id']] # 
+    
+    # -------------------------------------------------------------------------
+    
+    viewer.layers.pop(0)
+    viewer.add_image(
+        cellViewer.cell_data['display'],
+        name='Cell #' + str(cellViewer.cell_data['cell_id']) + ' MyoII',
+        contrast_limits = [0, np.quantile(myoii, 0.999)]
+        )
+    
+    # ------------------------------------------------------------------------- 
+    
+    line1.set_xdata(cellViewer.cell_data['timepoint'])
+    line1.set_ydata(cellViewer.cell_data['myoii_intden'])
+    ax1.relim()
+    ax1.autoscale_view()
+    
+    line2.set_xdata(cellViewer.cell_data['timepoint'])
+    line2.set_ydata(cellViewer.cell_data['area'])
+    ax2.relim()
+    ax2.autoscale_view()
+    
+    ax1.set_title('Cell #' + str(cellViewer.cell_data['cell_id']) + ' MyoII & cell area')
+
+    # ------------------------------------------------------------------------- 
+
+    plot_data.pulse1_ti.min = np.min(cellViewer.cell_data['timepoint'])-1
+    plot_data.pulse1_ti.max = np.max(cellViewer.cell_data['timepoint'])
+    plot_data.pulse1_ti.value = np.min(cellViewer.cell_data['timepoint'])-1
+    
+    plot_data.pulse1_tf.min = np.min(cellViewer.cell_data['timepoint'])-1
+    plot_data.pulse1_tf.max = np.max(cellViewer.cell_data['timepoint'])
+    plot_data.pulse1_tf.value = np.min(cellViewer.cell_data['timepoint'])-1
+      
+    
+#%%
+
+plot_data.native.layout().addWidget(FigureCanvas(cell_fig)) 
+
+viewer = cellViewer()
+
+viewer.add_image(
+    cellViewer.cell_data['display'],
+    name='Cell #' + str(cellViewer.cell_data['cell_id']) + ' MyoII',
+    contrast_limits = [0, np.quantile(myoii, 0.999)]
+    )
+
+viewer.window.add_dock_widget(plot_data, area='bottom', name='widget') 
+
+test = cellViewer.pulse_data
